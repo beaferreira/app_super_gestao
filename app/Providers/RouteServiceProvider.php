099de1a4501e8107f60c20a78df3d2fbef1561a2
beaferreira <triszferreira@gmail.com>
2022-10-27 22:04:15 -0300

    parametros do controlador p view

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -1,43 +1,47 @@
 <?php
-
+ 
 namespace App\Providers;
-
+ 
 use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
-
+ 
 class RouteServiceProvider extends ServiceProvider
 {
     /**
      * The path to the "home" route for your application.
      *
-     * Typically, users are redirected here after authentication.
+     * This is used by Laravel authentication to redirect users after login.
      *
      * @var string
      */
     public const HOME = '/home';
-
+ 
+    protected $namespace = 'App\Http\Controllers';
+ 
     /**
-     * Define your route model bindings, pattern filters, and other route configuration.
+     * Define your route model bindings, pattern filters, etc.
      *
      * @return void
      */
     public function boot()
     {
         $this->configureRateLimiting();
-
+ 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
+            Route::prefix('api')
+                ->namespace($this->namespace)
+                ->middleware('api')
                 ->group(base_path('routes/api.php'));
-
+ 
             Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
-
+ 
     /**
      * Configure the rate limiters for the application.
      *
